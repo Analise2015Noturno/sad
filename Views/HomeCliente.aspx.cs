3f6e6f9fe91f6766543effd85c8ba03c94ba756e
Wilian <wpio@hotmail.com>
2016-10-29 11:38:18 -0200

    Inclusao de tela HomeCliente

--- Views/HomeCliente.aspx.cs
+++ Views/HomeCliente.aspx.cs
@@ -8,7 +8,11 @@ using System.Web.UI.WebControls;
 public partial class HomeCliente : System.Web.UI.Page
 {
     protected void Page_Load(object sender, EventArgs e)
-{
+    {
+       
+        Problema problema = new Problema();
+        grdDados.DataSource = problema.Localiza();
+        grdDados.DataBind();
 
-}
+    }
 }
